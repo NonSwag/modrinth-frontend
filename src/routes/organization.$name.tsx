025558bcfd1fbac54e224d79dd8cbc3f2077ad4a
david <mail@nonswag.dev>
2026-06-20 07:39:25 +0200

    format

--- src/routes/organization.$name.tsx
+++ src/routes/organization.$name.tsx
@@ -1,9 +1,9 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute } from "@tanstack/react-router";
 
-export const Route = createFileRoute('/organization/$name')({
-  component: RouteComponent,
-})
+export const Route = createFileRoute("/organization/$name")({
+	component: RouteComponent,
+});
 
 function RouteComponent() {
-  return <div>Hello "/organization/$name"!</div>
+	return <div>Hello "/organization/$name"!</div>;
 }
